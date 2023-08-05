db4baea9ed43ee4a3b580dd1479fed0254d02bf0
Michael-Maina <michaelmaina02@gmail.com>
2023-08-05 05:51:49 +0300

    Added task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -24,5 +24,21 @@ class TestGithubOrgClient(unittest.TestCase):
         """
         api_url = f'https://api.github.com/orgs/{org_name}'
         mocked_inst = GithubOrgClient(org_name)
-        mocked_inst.org()
+        result = mocked_inst.org()
         mock_json.assert_called_once_with(api_url)
+
+    def test_public_repos_url(self):
+        """
+        Test the public_repos_url method
+        """
+        with patch("client.GithubOrgClient.org",
+                   new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = {
+                'repos_url': "https://api.github.com/users/google/repos",
+            }
+            org_name = "google"
+            client = GithubOrgClient(org_name)
+            result = client._public_repos_url
+            self.assertEqual(
+                result, "https://api.github.com/users/google/repos"
+            )
